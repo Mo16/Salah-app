d0f4c0f94dabe880f3546bba2923b897e0b9f121
Mo16 <mosminecraft4@gmail.com>
2020-12-28 20:00:44 +0000

    Update scraperoop.js

--- models/scraperoop.js
+++ models/scraperoop.js
@@ -5,36 +5,17 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-var masjidnoorData,
-  jamimosqueData,
-  masjidhidayaData,
-  didsburymosqueData,
-  portsmouthcentralmosqueData,
-  eastlondonmosqueData,
-  finsburyparkmosqueData,
-  glasgowCentralData,
-  filePath = "public/data/mosqueData.json";
 
 class Mosque {
   constructor(
-    name,
-    url,
-    fajr,
-    zuhr,
-    asr,
-    maghrib,
-    isha,
-    longitude,
-    latitude,
-    dropdownid
-  ) {
+    name, url, fajr, zuhr, asr, maghrib, esha, longitude, latitude, dropdownid) {
     this.name = name;
     this.url = url;
     this.fajr = fajr;
     this.zuhr = zuhr;
     this.asr = asr;
     this.maghrib = maghrib;
-    this.isha = isha;
+    this.esha = esha;
     this.latitude = latitude;
     this.longitude = longitude;
     this.dropdownid = dropdownid;
@@ -54,21 +35,20 @@ class Mosque {
 
   async getTimes(page) {
     try {
-      let browser = await puppeteer.launch();
-      const page = await browser.newPage();
       await page.goto(this.url);
       await page.waitForSelector(this.fajr);
-      // await page.waitForSelector(this.zuhr);
-      // await page.waitForSelector(this.asr);
-      // await page.waitForSelector(this.maghrib);
-      // await page.waitForSelector(this.esha);
+      await page.waitForSelector(this.zuhr);
+      await page.waitForSelector(this.asr);
+      await page.waitForSelector(this.maghrib);
+      await page.waitForSelector(this.esha);
 
-      let data = await page.evaluate(() => {
-        let fajr = document.querySelector(this.fajr).innerText;
-        let zuhr = document.querySelector(this.fajr).innerText;
-        let asr = document.querySelector(this.fajr).innerText;
-        let maghrib = document.querySelector(this.fajr).innerText;
-        let esha = document.querySelector(this.fajr).innerText;
+
+      await page.evaluate(function work(f,z,a,m,e) {
+        let fajr = document.querySelector(f)
+        let zuhr = document.querySelector(z);
+        let asr = document.querySelector(a);
+        let maghrib = document.querySelector(m);
+        let esha = document.querySelector(e);
 
         return {
           city: this.city,
@@ -84,27 +64,27 @@ class Mosque {
           esha,
         };
       });
-
-      jamimosqueData = data;
-      console.log(data);
+      work(this.fajr,this.zuhr,this.asr,this.maghrib,this.esha)
+      console.log(work);
     } catch (err) {
       await page.close();
       console.log("Couldn't get data because: \n" + err);
     }
   }
+
 }
 
 var jamimosque = new Mosque(
   "jamimosque",
-  "https://centralmosque.co.uk",
-  "#content-section-2 > div > div > div.col-md-5.columns > div > div.col-md-12.simple-column > div > div > table > tbody > tr:nth-child(7) > td.masjidnow-salah-time-iqamah.masjidnow-isha",
-  "#content-section-2 > div > div > div.col-md-5.columns > div > div.col-md-12.simple-column > div > div > table > tbody > tr:nth-child(7) > td.masjidnow-salah-time-iqamah.masjidnow-isha",
-  "#content-section-2 > div > div > div.col-md-5.columns > div > div.col-md-12.simple-column > div > div > table > tbody > tr:nth-child(7) > td.masjidnow-salah-time-iqamah.masjidnow-isha",
-  "#content-section-2 > div > div > div.col-md-5.columns > div > div.col-md-12.simple-column > div > div > table > tbody > tr:nth-child(7) > td.masjidnow-salah-time-iqamah.masjidnow-isha",
-  "#content-section-2 > div > div > div.col-md-5.columns > div > div.col-md-12.simple-column > div > div > table > tbody > tr:nth-child(7) > td.masjidnow-salah-time-iqamah.masjidnow-isha",
+  "http://portsmouthjamimosque.co.uk/",
+  "body > main > section.section.background-dark > div > div > div.s-12.m-12.l-4.margin-m-bottom.prayer-times > div:nth-child(5) > div:nth-child(3)",
+  "body > main > section.section.background-dark > div > div > div.s-12.m-12.l-4.margin-m-bottom.prayer-times > div:nth-child(6) > div:nth-child(3)",
+  "body > main > section.section.background-dark > div > div > div.s-12.m-12.l-4.margin-m-bottom.prayer-times > div:nth-child(7) > div:nth-child(3)",
+  "body > main > section.section.background-dark > div > div > div.s-12.m-12.l-4.margin-m-bottom.prayer-times > div:nth-child(8) > div:nth-child(2)",
+  "body > main > section.section.background-dark > div > div > div.s-12.m-12.l-4.margin-m-bottom.prayer-times > div:nth-child(9) > div:nth-child(3)",
   1,
   2,
-  "hello"
+  "ports jami"
 );
 
 jamimosque.openSite();
