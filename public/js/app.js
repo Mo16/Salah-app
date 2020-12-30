11a22a19f49ed0694904fe1481b0c54a3c12c4ce
mo3r99 <mo3r99@gmail.com>
2020-12-30 01:24:14 +0000

    fixed carousel indicators

--- public/js/app.js
+++ public/js/app.js
@@ -203,20 +203,19 @@ clock();
 
 function checkCarouselScroll() {
     let carousel = document.querySelector('.horizontal-scroll');
-
-    console.log(carousel.scrollLeft);
+    let maxScrollArea = window.innerWidth/0.6;
 
     if (carousel.scrollLeft < 3) {
         document.querySelector('.dot1').classList.add('active-dot');
         document.querySelector('.dot2').classList.remove('active-dot');
         document.querySelector('.dot3').classList.remove('active-dot');
         console.log('1')
-    } else if (3 < carousel.scrollLeft && carousel.scrollLeft < 1067) {
+    } else if (3 < carousel.scrollLeft && carousel.scrollLeft < (maxScrollArea/3)*2 ) {
         document.querySelector('.dot1').classList.remove('active-dot');
         document.querySelector('.dot2').classList.add('active-dot');
         document.querySelector('.dot3').classList.remove('active-dot');
         console.log('2')
-    } else if (carousel.scrollLeft > 2100) {
+    } else if ( carousel.scrollLeft > (maxScrollArea/3)*2 ) {
         document.querySelector('.dot1').classList.remove('active-dot');
         document.querySelector('.dot2').classList.remove('active-dot');
         document.querySelector('.dot3').classList.add('active-dot');
