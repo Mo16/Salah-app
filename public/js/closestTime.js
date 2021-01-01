b94a04f8d3b01b02ed873259e55634063d782f84
mo3r99 <mo3r99@gmail.com>
2021-01-01 15:56:43 +0000

    Fixed some issues with closest time

--- public/js/closestTime.js
+++ public/js/closestTime.js
@@ -17,8 +17,6 @@ function whichNamaz(data) {
       if (namazTime < currentTime) {
         counter++;
       }
-
-      console.log(`counter is ${counter}`)
     }
 
     if (counter === 6) {
@@ -56,34 +54,99 @@ function changeHTML(namaz,nextSalah,data){
   document.querySelector(`.${namaz}-jamat-time-name`).style.color = selectedColor;
   document.querySelector(`.${namaz}-start-time-name`).style.color = selectedColor;
   document.querySelector(`.${namaz}`).style.color = selectedColor;
-  calculateTimeRemaining(data[nextSalah]);
-  console.log(`changed ${namaz} to ${data[nextSalah]}`);
+
+  let remainingTime = calculateTimeRemaining(data[nextSalah]);
+  let remainingHours = remainingTime[0];
+  let remainingMins = remainingTime[1];
+  document.querySelector('.next-salah-in').innerHTML = `In ${remainingHours} hours and ${remainingMins} minutes`;
+
+  if (remainingHours === 0){
+    document.querySelector('.next-salah-in').innerHTML = `In ${remainingMins} minutes`;
+  }
+
+  closestJamat();
 }
 
 function calculateTimeRemaining(time) {
-    let today = new Date();
+  if (time[5] === undefined) { //e.g. When time is in form 3:30
+    let time1 = time[1];
+    let time2 = time[2];
+    let time3 = time[3];
+    let time4 = time[4];
+    
+    timeArray = ['0', time1, time2, time3, time4];
+    time = timeArray.join('');
+  } else if (time[5] === 'a' || time[5] === 'p') { //e.g. When time is in form 07:30am
+    let time1 = time[1];
+    let time2 = time[2];
+    let time3 = time[3];
+    let time4 = time[4];
+    let time5 = time[5];
+    
+    timeArray = [time1, time2, time3, time4, time5];
+    time = timeArray.join('');
+  } else if (time [4] === 'a' || time[4] === 'p') { //e.g. When time is in form 7:30am
+    let time1 = time[1]; //7
+    let time2 = time[2]; //:
+    let time3 = time[3]; //3
+    let time4 = time[4]; //0
+    
+    timeArray = ['0', time1, time2, time3, time4];
+    time = timeArray.join('');
+  }
 
-    let namazHour = parseInt(time[0] + time[1]);
-    let namazMins = parseInt(time[3] + time[4]);
+  let today = new Date();
 
-    var remainingHours = namazHour - today.getHours();
-    var remainingMins = namazMins - today.getMinutes();
+  let namazHour = parseInt(time[0] + time[1]);
+  let namazMins = parseInt(time[3] + time[4]);
 
-    if (remainingMins < 0) {
-        remainingMins = 60 + remainingMins;
-    }
+  var remainingHours = namazHour - today.getHours();
+  var remainingMins = namazMins - today.getMinutes();
 
-    if (remainingHours < 0) {
-        remainingHours = remainingHours + 24;
-    }
+  if (remainingMins < 0) {
+      remainingMins = 60 + remainingMins;
+  }
 
-    document.querySelector('.next-salah-in').innerHTML = `In ${remainingHours} hours and ${remainingMins} minutes`;
+  if (remainingHours < 0) {
+    remainingHours = remainingHours + 24;
+  }
 
-    if (remainingHours === 0){
-      document.querySelector('.next-salah-in').innerHTML = `In ${remainingMins} minutes`;
-    }
+  return [remainingHours, remainingMins];
 }
 
-function closestJamat(prayer) {
+function closestJamat() {
+  var timeRemaining;
   
+  switch (prayer) {
+    case 'fajr':
+      document.querySelector('.next-jamat-name').innerHTML = 'Fajr:';
+      document.querySelector('.next-jamat-time').innerHTML = document.querySelector('.j-fajr').innerHTML;
+      timeRemaining = calculateTimeRemaining(document.querySelector('.j-fajr').innerHTML);
+      document.querySelector('.next-jamat-in').innerHTML = `In ${timeRemaining[0]} hours and ${timeRemaining[1]} minutes at ${document.querySelector('select').value}`;
+      break;
+    case 'zuhr':
+      document.querySelector('.next-jamat-name').innerHTML = 'Dhuhr:';
+      document.querySelector('.next-jamat-time').innerHTML = document.querySelector('.j-zuhr').innerHTML;
+      timeRemaining = calculateTimeRemaining(document.querySelector('.j-zuhr').innerHTML);
+      document.querySelector('.next-jamat-in').innerHTML = `In ${timeRemaining[0]} hours and ${timeRemaining[1]} minutes at ${document.querySelector('select').value}`;
+      break;
+    case 'asr':
+      document.querySelector('.next-jamat-name').innerHTML = 'Asr:';
+      document.querySelector('.next-jamat-time').innerHTML = document.querySelector('.j-asr').innerHTML;
+      timeRemaining = calculateTimeRemaining(document.querySelector('.j-asr').innerHTML);
+      document.querySelector('.next-jamat-in').innerHTML = `In ${timeRemaining[0]} hours and ${timeRemaining[1]} minutes at ${document.querySelector('select').value}`;
+      break;
+    case 'maghrib':
+      document.querySelector('.next-jamat-name').innerHTML = 'Mahgrib:';
+      document.querySelector('.next-jamat-time').innerHTML = document.querySelector('.j-maghrib').innerHTML;
+      timeRemaining = calculateTimeRemaining(document.querySelector('.j-maghrib').innerHTML);
+      document.querySelector('.next-jamat-in').innerHTML = `In ${timeRemaining[0]} hours and ${timeRemaining[1]} minutes at ${document.querySelector('select').value}`;
+      break;
+    case 'isha':
+      document.querySelector('.next-jamat-name').innerHTML = 'Isha:';
+      document.querySelector('.next-jamat-time').innerHTML = document.querySelector('.j-isha').innerHTML;
+      timeRemaining = calculateTimeRemaining(document.querySelector('.j-isha').innerHTML);
+      document.querySelector('.next-jamat-in').innerHTML = `In ${timeRemaining[0]} hours and ${timeRemaining[1]} minutes at ${document.querySelector('select').value}`;
+      break;
+  }
 }
